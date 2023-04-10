250f53a721ad5ed8882e7672bbb64622f96d6263
manzaero <ligoero@gmail.com>
2023-04-10 18:41:33 +0500

    refact components footer

--- src/components/header/TheBurger.vue
+++ src/components/header/TheBurger.vue
@@ -9,9 +9,10 @@
       <div class="menu-box__flex">
         <div class="menu-box__main">
           <li><a class="menu__item" href="#">Каталог</a></li>
-          <li><a class="menu__item" href="#">Комнаты</a></li>
-          <li><a class="menu__item" href="#">Дизайн-проекты</a></li>
-          <li><a class="menu__item" href="#">Мебель на заказ</a></li>
+          <li><a class="menu__item" href="#">Доставка</a></li>
+          <li><a class="menu__item" href="#">Оплата</a></li>
+          <li><a class="menu__item" href="#">Контакты</a></li>
+          <li><a class="menu__item" href="#">О компании</a></li>
           <hr>
           <li class="menu__flex">
             <div class="menu__icon">
@@ -19,12 +20,6 @@
             </div>
             <span class="menu__item" href="#">Вход / Регистрация</span>
           </li>
-          <hr>
-          <li><a class="menu__contacts" href="#">Санкт-Петербург <i class="arrow down"></i></a></li>
-          <li><a class="menu__contacts" href="#">О нас</a></li>
-          <li><a class="menu__contacts" href="#">Наши салоны</a></li>
-          <li><a class="menu__contacts" href="#">Оплата и доставка</a></li>
-          <li><a class="menu__contacts" href="#">Для бизнеса</a></li>
         </div>
       </div>
     </ul>
